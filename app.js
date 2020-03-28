747db9914a3945b144021a52449db180bdca1599
Eira Teufel <sv.tolstyakova@gmail.com>
2020-03-28 21:18:37 +0300

    mongoose server connected

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 const path = require('path');
 const usersRouter = require('./routes/users.js');
 const cardsRouter = require('./routes/cards.js');
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', cardsRouter);

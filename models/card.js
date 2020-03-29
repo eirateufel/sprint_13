0dcf9762a346cfacba7767dcb33c74071e935b85
Eira Teufel <sv.tolstyakova@gmail.com>
2020-03-30 00:04:32 +0300

    link validation

--- models/card.js
+++ models/card.js
@@ -9,7 +9,14 @@ const cardSchema = new mongoose.Schema({
   },
   link: {
     type: String,
-    required: true
+    required: true,
+    validate: {
+      validator: function(v) {
+        const urlRegex = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/;
+        return urlRegex.test(v);
+      },
+      message: props => `${props.value} is not a valid url!`
+    }
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,

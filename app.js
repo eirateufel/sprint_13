bb34dea33ac14dafc4954657a96939f47499cc2a
Eira Teufel <sv.tolstyakova@gmail.com>
2020-03-29 23:56:09 +0300

    + bodyParser and middleware user id

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -13,10 +14,18 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
     useFindAndModify: false
 });
 
+app.use((req, res, next) => {
+  req.user = {
+      _id: '5e80f03e42ed865048e4e58a' // вставьте сюда _id созданного в предыдущем пункте пользователя
+  };
+
+  next();
+});
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
 app.use('/', cardsRouter);
 app.use('/', usersRouter);
 
 app.listen(PORT, () => {
-  // console.log('Im running');
+   console.log('Im running');
 });

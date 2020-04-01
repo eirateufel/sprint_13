ede206a54f93bf6084d2c4ff052cc7cca761d931
Eira Teufel <sv.tolstyakova@gmail.com>
2020-04-01 19:14:29 +0300

    middlwares added to router

--- routes/cards.js
+++ routes/cards.js
@@ -1,10 +1,11 @@
 const cardsRouter = require('express').Router();
 const { getCards, createCard, removeCard } = require('../controllers/cards');
+const { doesCardExist, checkOwner } = require('../middlware/cards');
 
 cardsRouter.get('/cards', getCards);
 
 cardsRouter.post('/cards', createCard);
 
-cardsRouter.delete('/cards/:cardId', removeCard);
+cardsRouter.delete('/cards/:cardId', doesCardExist, checkOwner, removeCard);
 
 module.exports = cardsRouter;

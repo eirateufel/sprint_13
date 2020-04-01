70865febe70c9bc5a4f2cd2b712db9d9902b958d
Eira Teufel <sv.tolstyakova@gmail.com>
2020-04-01 19:07:32 +0300

    middlware folder added

---
+++ middlware/cards.js
@@ -0,0 +1,23 @@
+const Card = require('../models/card');
+
+module.exports.doesCardExist = (req, res, next) => {
+  Card.findById(req.params.cardId)
+		.then((card) => {
+      if (card){next();}
+      else{res.status(404).send({ message: 'Карточка, которую вы пытаетесь удалить, не найдена' });}
+    })
+		.catch((err) => {
+        res.status(500).send({ message: err.message });
+		});
+};
+
+module.exports.checkOwner = (req, res, next) => {
+  Card.findById(req.params.cardId)
+		.then((card) => {
+      if (toString(card.owner) === toString(req.user._id)){next();}
+      else{res.status(403).send({ message: 'Недостаточно прав' });}
+    })
+		.catch((err) => {
+        res.status(500).send({ message: err.message });
+		});
+}

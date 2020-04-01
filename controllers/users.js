f6f7faacf72b39dfb71806a57df63c54d21aa0c6
Eira Teufel <sv.tolstyakova@gmail.com>
2020-04-01 15:12:11 +0300

    error message modified

--- controllers/users.js
+++ controllers/users.js
@@ -18,5 +18,12 @@ module.exports.createUser = (req, res) => {
 
 	User.create({ name, about, avatar })
 		.then((user) => res.send({ data: user }))
-		.catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+		.catch((err) => {
+      if (err.name === 'ValidationError'){
+        res.status(400).send({ message: `Введенные данные некорректны: ${err.errors.name.message}` });
+      }
+      else {
+        res.status(500).send({ message: err });
+      }
+    });
 };
